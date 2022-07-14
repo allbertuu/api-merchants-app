cc3d8e9da02c4c61869674f85562ee5f2f2104fc
Alberto <albertovinicius3@gmail.com>
2022-07-14 10:41:15 -0300

    fix error server

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://merchants-app-allbertuu.vercel.app/");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://merchants-app-allbertuu.vercel.app/");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Access-Control-Allow-Headers"
+//   );
+//   next();
+// });
 
 app.get("/", (req, res) => {
   getMerchants()
